4fa74d33abb8a2e04d7b7736ceec9343f22ca6c6
Reyhan Jovie <reyhanjovie01@gmail.com>
2024-06-25 21:13:00 +0700

    add comment

--- modules/transaction/adapter/user.go
+++ modules/transaction/adapter/user.go
@@ -6,11 +6,15 @@ import (
 	userDomain "simple-modular/modules/user/domain"
 )
 
+// depend to userDomain
 type userRepository interface {
 	FindUserById(ctx context.Context, userId int) (user userDomain.User, err error)
 	UpdateBalance(ctx context.Context, userId int, balance int) (err error)
 }
 
+// untuk interaksi antar cross domain, kita butuh "adapter"
+// adapter ini akan bertugas sebagai proxy ke domain lain
+// tujuannya adalah agar saat terjadinya cara komunikasi antar domain, maka cukup mengubah adapter saja
 type userAdapter struct {
 	repo userRepository
 }
